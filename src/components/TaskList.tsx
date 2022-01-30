2b9fb82ac7ae5857716872a710cc9aa40f5705ae
Michel John <michel.santos@dataside.com.br>
2022-01-30 17:01:49 -0300

    :tada: Challenge completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,37 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    function getRandomInt() {
+      const min = Math.ceil(1);
+      const max = Math.floor(100);
+
+      return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    const task = {
+      id: getRandomInt(),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+
+    setTasks([...tasks, task]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const updatedTasks = tasks.map(task => task.id === id
+      ? {...task, isComplete: !task.isComplete}
+      : task);
+
+    setTasks(updatedTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id !== id));
   }
 
   return (
